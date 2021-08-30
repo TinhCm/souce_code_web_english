fd54a2e4a1713a6ba792f17b0cea6a0d862183da
TinhCm <66770122+TinhCm@users.noreply.github.com>
2021-08-30 21:47:53 +0700

    fix

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// var API_tu_vung = 'https://demo-apps-en.herokuapp.com/list'
-// var API_ngu_phap = 'https://demo-apps-en.herokuapp.com/ngu_phap'
-// var API_gop_y = "https://demo-apps-en.herokuapp.com/gop_y"
-// var API_dang_nhap ='https://demo-apps-en.herokuapp.com/user'
-// var API_dang_ki ='https://demo-apps-en.herokuapp.com/user'
+var API_tu_vung = 'https://demo-apps-en.herokuapp.com/list'
+var API_ngu_phap = 'https://demo-apps-en.herokuapp.com/ngu_phap'
+var API_gop_y = "https://demo-apps-en.herokuapp.com/gop_y"
+var API_dang_nhap ='https://demo-apps-en.herokuapp.com/user'
+var API_dang_ki ='https://demo-apps-en.herokuapp.com/user'
 
-var API_tu_vung = 'http://localhost:3000/list'
-var API_ngu_phap = 'http://localhost:3000/ngu_phap'
-var API_gop_y = "http://localhost:3000/gop_y"
-var API_dang_nhap ='http://localhost:3000/user'
-var API_dang_ki ='http://localhost:3000/user'
+// var API_tu_vung = 'http://localhost:3000/list'
+// var API_ngu_phap = 'http://localhost:3000/ngu_phap'
+// var API_gop_y = "http://localhost:3000/gop_y"
+// var API_dang_nhap ='http://localhost:3000/user'
+// var API_dang_ki ='http://localhost:3000/user'
 
 //Phần API_tu_vung từ vựng
 function batDau_tu_vung()
