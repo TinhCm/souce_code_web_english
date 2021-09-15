1d77f830a4a0b2e6df4f07ba1cf1a026b2ab6267
TinhCm <66770122+TinhCm@users.noreply.github.com>
2021-09-15 15:15:13 +0700

    cap nhat giao dien 3.0

--- index.js
+++ index.js
@@ -643,6 +643,7 @@ function getGio() {
 }
 
 //Học bài
+//Học bài
 function batDau_hoc_bai() {
     xuLi_hoc_bai();
 }
@@ -656,25 +657,10 @@ function layDuLieu_hoc_bai(callback) {
         .then(callback);
 }
 
-function taoDuLieu_hoc_bai(data, callback) {
-    var opption = {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    }
-
-    fetch(API_hoc_bai, opption)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(callback);
-}
-
 function xuLi_hoc_bai() {
-    var xuLi_hoc_bai = document.querySelector('.hocBai--create');
-    xuLi_hoc_bai.onclick = function() {
+    //Lever1
+    var hocBai_kiem_tra = document.querySelector('.hocBai_kiem_tra');
+    hocBai_kiem_tra.onclick = function() {
         var input1_tu_hocBai = document.querySelector('.input1_tu_hocBai').value;
         var input2_nghia_hocBai = document.querySelector('.input2_nghia_hocBai').value;
         var hocBai_list_input1_check = document.querySelector('.hocBai_list-input1_check');
@@ -697,6 +683,9 @@ function xuLi_hoc_bai() {
             if (loc_user_hoc_bai_ton_tai2 == true) {
                 if (loc_user_hoc_bai_ton_tai3 == true) {
                     hocBai_list_input1_check.innerHTML = "Đúng";
+                    setTimeout(() => {
+                        location.reload();
+                    }, 1000);
                 } else {
                     hocBai_list_input1_check.innerHTML = "Sai";
                 }
@@ -705,11 +694,170 @@ function xuLi_hoc_bai() {
             }
         }
     }
-}
 
-//Xử lí động
-var hocBai_bat_dau = document.querySelector('.hocBai_bat_dau1');
+    //Lever2
+    var hocBai_kiem_tra2 = document.querySelector('.hocBai_kiem_tra2');
 
-hocBai_bat_dau.onclick = function() {
-    location.reload();
+    window.onload = function() {
+        var input1_tu_hocBai2 = document.querySelector('.input1_tu_hocBai2');
+
+        layDuLieu_hoc_bai(check_ket_qua_lever2);
+
+        function check_ket_qua_lever2(list) {
+            var loc_user_hoc_bai_ton_tai = list.filter(function(lists) {
+                return lists.user === document.cookie;
+            })
+
+            const check_ket_qua_lever2_length = loc_user_hoc_bai_ton_tai.length;
+            console.log(check_ket_qua_lever2_length);
+
+            var check_ket_qua_lever2_random = (Math.floor(Math.random() * check_ket_qua_lever2_length));
+            console.log(check_ket_qua_lever2_random);
+
+            var check_ket_qua_lever2_kq_random = (loc_user_hoc_bai_ton_tai[check_ket_qua_lever2_random].tu_vung);
+
+            input1_tu_hocBai2.innerHTML = check_ket_qua_lever2_kq_random;
+
+        }
+    }
+
+    //Xem đáp án
+    var input1_tu_hocBai2_DA = document.querySelector('.input1_tu_hocBai2_DA');
+    var hocBai_dap_an2 = document.querySelector('.hocBai_dap_an2');
+    var input1_tu_hocBai2 = document.querySelector('.input1_tu_hocBai2');
+    var input2_nghia_hocBai2 = document.querySelector('.input2_nghia_hocBai2').value;
+
+    hocBai_dap_an2.onclick = function() {
+        layDuLieu_hoc_bai(check_ket_qua_lever2);
+
+        function check_ket_qua_lever2(list) {
+            var loc_user_hoc_bai_ton_tai = list.filter(function(lists) {
+                return lists.user === document.cookie;
+            })
+
+            var loc_user_hoc_bai_ton_tai2 = loc_user_hoc_bai_ton_tai.find(function(listss) {
+                return listss.tu_vung === input1_tu_hocBai2.innerHTML;
+            })
+
+            console.log(loc_user_hoc_bai_ton_tai2.nghia);
+            input1_tu_hocBai2_DA.innerHTML = loc_user_hoc_bai_ton_tai2.nghia;
+        }
+    }
+
+    //Check đáp án
+    hocBai_kiem_tra2.onclick = function() {
+        var input1_tu_hocBai2 = document.querySelector('.input1_tu_hocBai2');
+        var input2_nghia_hocBai2 = document.querySelector('.input2_nghia_hocBai2').value;
+        var hocBai_list_input1_check2 = document.querySelector('.hocBai_list-input1_check2');
+
+        layDuLieu_hoc_bai(check_ket_qua_lever2);
+
+        function check_ket_qua_lever2(list) {
+            var loc_user_hoc_bai_ton_tai = list.filter(function(lists) {
+                return lists.user === document.cookie;
+            })
+
+            var loc_user_hoc_bai_ton_tai2 = loc_user_hoc_bai_ton_tai.some(function(listss) {
+                return listss.tu_vung === input1_tu_hocBai2.innerHTML;
+            })
+
+            var loc_user_hoc_bai_ton_tai3 = loc_user_hoc_bai_ton_tai.some(function(listss) {
+                return listss.nghia === input2_nghia_hocBai2;
+            })
+
+            if (loc_user_hoc_bai_ton_tai3 == true) {
+                hocBai_list_input1_check2.innerHTML = "Đúng";
+                setTimeout(() => {
+                    location.reload();
+                }, 1000);
+
+            } else {
+                hocBai_list_input1_check2.innerHTML = "Sai";
+            }
+
+        }
+    }
+
+    //Lever 3
+    var hocBai_kiem_tra3 = document.querySelector('.hocBai_kiem_tra3');
+    var hocBai_bat_dau3 = document.querySelector('.hocBai_bat_dau3');
+    hocBai_bat_dau3.onclick = function() {
+        var input1_tu_hocBai3 = document.querySelector('.input1_tu_hocBai3');
+
+        layDuLieu_hoc_bai(check_ket_qua_lever3);
+
+        function check_ket_qua_lever3(list) {
+            var loc_user_hoc_bai_ton_tai = list.filter(function(lists) {
+                return lists.user === document.cookie;
+            })
+
+            const check_ket_qua_lever3_length = loc_user_hoc_bai_ton_tai.length;
+            console.log(check_ket_qua_lever3_length);
+
+            var check_ket_qua_lever3_random = (Math.floor(Math.random() * check_ket_qua_lever3_length));
+            console.log(check_ket_qua_lever3_random);
+
+            var check_ket_qua_lever3_kq_random = (loc_user_hoc_bai_ton_tai[check_ket_qua_lever3_random].nghia);
+
+            input1_tu_hocBai3.innerHTML = check_ket_qua_lever3_kq_random;
+
+        }
+    }
+
+    //Xem đáp án
+    var input1_tu_hocBai3_DA = document.querySelector('.input1_tu_hocBai3_DA');
+    var hocBai_dap_an3 = document.querySelector('.hocBai_dap_an3');
+    var input1_tu_hocBai3 = document.querySelector('.input1_tu_hocBai3');
+    var input2_nghia_hocBai3 = document.querySelector('.input2_nghia_hocBai3').value;
+
+    hocBai_dap_an3.onclick = function() {
+        layDuLieu_hoc_bai(check_ket_qua_lever3);
+
+        function check_ket_qua_lever3(list) {
+            var loc_user_hoc_bai_ton_tai = list.filter(function(lists) {
+                return lists.user === document.cookie;
+            })
+
+            var loc_user_hoc_bai_ton_tai2 = loc_user_hoc_bai_ton_tai.find(function(listss) {
+                return listss.nghia === input1_tu_hocBai3.innerHTML;
+            })
+
+            console.log(loc_user_hoc_bai_ton_tai2.tu_vung);
+            input1_tu_hocBai3_DA.innerHTML = loc_user_hoc_bai_ton_tai2.tu_vung;
+        }
+    }
+
+    //Check đáp án
+    hocBai_kiem_tra3.onclick = function() {
+        var input1_tu_hocBai3 = document.querySelector('.input1_tu_hocBai3');
+        var input2_nghia_hocBai3 = document.querySelector('.input2_nghia_hocBai3').value;
+        var hocBai_list_input1_check3 = document.querySelector('.hocBai_list-input1_check3');
+
+        layDuLieu_hoc_bai(check_ket_qua_lever3);
+
+        function check_ket_qua_lever3(list) {
+            var loc_user_hoc_bai_ton_tai = list.filter(function(lists) {
+                return lists.user === document.cookie;
+            })
+
+            var loc_user_hoc_bai_ton_tai2 = loc_user_hoc_bai_ton_tai.some(function(listss) {
+                return listss.nghia === input1_tu_hocBai3.innerHTML;
+            })
+
+            var loc_user_hoc_bai_ton_tai3 = loc_user_hoc_bai_ton_tai.some(function(listss) {
+                return listss.tu_vung === input2_nghia_hocBai3;
+            })
+
+            if (loc_user_hoc_bai_ton_tai3 == true) {
+                hocBai_list_input1_check3.innerHTML = "Đúng";
+                setTimeout(() => {
+                    location.reload();
+                }, 1000);
+
+            } else {
+                hocBai_list_input1_check3.innerHTML = "Sai";
+            }
+
+        }
+    }
 }
